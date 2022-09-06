9d34fe4cc3679c5ea5ebe020f8114ab67dc99a6a
georgirtodorov <georgi.r.todorov@gmail.com>
2022-09-06 13:04:32 +0300

    url fixes

--- resources/frontend/angular2/src/app/services/pages/settings/departments.service.ts
+++ resources/frontend/angular2/src/app/services/pages/settings/departments.service.ts
@@ -9,7 +9,7 @@ import {UrlBuilderService} from '../../helpers/url-builder/url-builder.service'
 export class DepartmentsService {
 
     private headers;
-    private api_url = this.url.base_url + '/departments/';
+    private api_url = this.url.base_url + '/departments';
 
     constructor(
         private http: HttpClient,
@@ -33,11 +33,11 @@ export class DepartmentsService {
         let options = {
             headers: this.headers
         }
-        return this.http.put<any>(this.api_url + id, department, options);
+        return this.http.put<any>(this.api_url + '/' + id, department, options);
     }
 
     public delete(department_id: number): Observable<any> {
-        return this.http.delete<any>(this.api_url + department_id, {headers: this.headers});
+        return this.http.delete<any>(this.api_url + '/' + department_id, {headers: this.headers});
     }
 }
 
