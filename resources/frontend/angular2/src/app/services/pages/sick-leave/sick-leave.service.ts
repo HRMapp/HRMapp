b275d6da97a51a4c9731ba63094b04235e24ac95
georgirtodorov <georgi.r.todorov@gmail.com>
2022-09-06 13:19:10 +0300

    another url fixes

--- resources/frontend/angular2/src/app/services/pages/sick-leave/sick-leave.service.ts
+++ resources/frontend/angular2/src/app/services/pages/sick-leave/sick-leave.service.ts
@@ -19,7 +19,7 @@ export class SickLeaveService {
         });
     }
 
-    private api_url = this.url.base_url + '/sick-leave/';
+    private api_url = this.url.base_url + '/sick-leave';
 
     public get(): Observable<any> {
         return this.http.get(this.api_url, {headers: this.headers});
@@ -34,14 +34,14 @@ export class SickLeaveService {
         let options = {
             headers: this.headers
         }
-        return this.http.put<any>(this.api_url + id, s_request, options);
+        return this.http.put<any>(this.api_url  + '/' + id, s_request, options);
     }
 
     public delete(id: number): Observable<any> {
-        return this.http.delete<any>(this.api_url + id, {headers: this.headers});
+        return this.http.delete<any>(this.api_url + '/' + id, {headers: this.headers});
     }
 
     public export(){
-        return this.http.get(this.api_url + 'export', {headers: this.headers});
+        return this.http.get(this.api_url + '/' + 'export', {headers: this.headers});
     }
 }
